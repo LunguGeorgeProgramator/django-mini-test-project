30ba8cdd14d616c2d36f221ad48117bc3f44b583
Lungu George <lungugeorge111@gmail.com>
2020-03-09 16:42:16 +0200

    set photo to be destroyed on delete action

--- media/models.py
+++ media/models.py
@@ -1,3 +1,5 @@
+import os
+
 from django.db import models
 from masina.models import Masini
 
@@ -5,3 +7,8 @@ from masina.models import Masini
 class Media(models.Model):
     media = models.FileField(upload_to="masina/")
     masina = models.ForeignKey(Masini, on_delete=models.CASCADE)
+
+    def delete(self):
+        if os.path.isfile(self.media.path):
+            os.remove(self.media.path)
+        super(Media, self).delete()
